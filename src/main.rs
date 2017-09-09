c80bbe0b3414a6fbe93601dcb462bdb3b6b51f71
Lewis Daly <lewis.daly@enabled.com.au>
2017-09-09 12:35:10 +0930

    adding basic getBalance structure

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,39 @@ extern crate clap;
 
 use clap::App;
 
-mod generate_address;
+mod api;
+mod api_commands;
+mod request;
+
+use request::Client;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    match matches.subcommand_name() {
-        Some("generate-address") => generate_address::generate_address(),
-        _  => println!("Other command was used")
+    //TODO: make a client or something!
+    let client = &Client { host: "google.com", port: 12345 };
+
+    let value = matches.value_of("balance");
+    println!("{:?}", value);
+
+    // match matches.subcommand_name() {
+    //     Some("generate-address") => api::generate_address(),
+    //     Some("balance") => api::balance(),
+    //     _  => println!("Other command was used")
+    // }
+
+    match matches.subcommand() {
+        ("generate-address", Some(matches)) => {
+            println!("Generating Address: {}", matches.value_of("seed").unwrap())
+        },
+        ("balance", Some(matches)) => {
+            println!("Checking balance: {}", matches.value_of("seed").unwrap());
+            //TODO: we can prompt user for seed here if we like!
+            let seed = matches.value_of("seed").unwrap();
+            api::balance(client, seed);
+        },
+        _ => println!("Other command was used")
     }
 
 }
--- todo.md
+++ todo.md
@@ -4,3 +4,8 @@
 - Parse cli arguments somehow
 - Read hostname from environment variables
 - investigate http libraries
+
+
+
+- implement a "getCommand" tool, similar to apiCommands.js
+- implement a "makeRequest" method, which actually talks to the api, refer to utils/makeRequest.js
