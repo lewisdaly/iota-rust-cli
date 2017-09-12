89f26c5512629c208177b187e40f94be9a3fcd15
Lewis Daly <lewis.daly@enabled.com.au>
2017-09-12 22:08:21 +0930

    work on serde json config

--- src/api.rs
+++ src/api.rs
@@ -33,15 +33,30 @@ pub fn tx_status(request: &IotaRequest, tx_hash: &str) {
     //TODO: we should probably make sure this response is the correct format
     //TODO: get the most recent solid milestone from node_response
     let mut tip = node_response["latestSolidSubtangleMilestone"].to_string();
-    //TODO: Deserialize properly. This will work for now
+
+    //TODO: Deserialize properly. This will do for now
     tip = str::replace(tip.as_str(), "\"", "");
     tip = str::replace(tip.as_str(), "\\", "");
 
-    // let tip = serde_json::to_string(tip_json.as_str()).unwrap();
     println!("{:?}", tip);
 
     let inclusion_command = api_commands::get_inclusion_states(tx_hash.to_owned(), tip.to_owned());
     let inclusion_response = request.make_request(inclusion_command.unwrap());
 
     println!("{:?}", inclusion_response.unwrap());
+}
+
+
+pub fn tx_show(request: &IotaRequest, tx_hash: &str) {
+    //First, perform getTrytes
+    let command = api_commands::get_trytes(tx_hash.to_owned());
+    let response = request.make_request(command.unwrap()).unwrap();
+
+    let trytes  = response["trytes"].to_string();
+
+    println!("{:?}", trytes);
+
+
+    //Then do rust lib magic
+
 }
--- src/api_commands.rs
+++ src/api_commands.rs
@@ -1,21 +1,26 @@
 #[allow(dead_code)]
 
+use serde_json;
+
 pub trait Command {
     //serialize the command for use in post request
     fn serialize(&self) -> String;
 }
 
 #[derive(Debug)]
+#[derive(Serialize)]
 pub struct CommandGetBalance {
     command: String,
     addresses: [String; 1],
     threshold: i32,
 }
 
+#[derive(Serialize)]
 pub struct CommandGetNodeInfo {
     command: String,
 }
 
+#[derive(Serialize)]
 pub struct CommandGetInclusionStates {
     command: String,
     transactions: [String; 1],
@@ -29,35 +34,26 @@ pub struct CommandGetTrytes {
 }
 
 impl Command for CommandGetBalance {
-    //TODO: don't serialize ourselves - use this: #[derive(Serialize, Deserialize)]
     fn serialize(&self) -> String {
-        let json = json!({
-            "command": self.command,
-            "addresses": self.addresses,
-            "threshold": self.threshold
-        });
-        json.to_string()
+        serde_json::to_string(&self).unwrap()
     }
 }
 
 impl Command for CommandGetNodeInfo {
     fn serialize(&self) -> String {
-        let json = json!({
-            "command": self.command,
-        });
-
-        json.to_string()
+        serde_json::to_string(&self).unwrap()
     }
 }
 
 impl Command for CommandGetInclusionStates {
     fn serialize(&self) -> String {
-        let json = json!({
-            "command": self.command,
-            "transactions": self.transactions,
-            "tips": self.tips,
-        });
-        json.to_string()
+        serde_json::to_string(&self).unwrap()
+    }
+}
+
+impl Command for CommandGetTrytes {
+    fn serialize(&self) -> String {
+        serde_json::to_string(&self).unwrap()
     }
 }
 
@@ -86,6 +82,13 @@ pub fn get_inclusion_states(transaction: String, tip: String) -> Option<Box<Comm
         transactions: transactions,
         tips: tips
     };
+
+    Some(Box::new(command))
+}
+
+pub fn get_trytes(hash: String) -> Option<Box<Command>> {
+    let hashes = [hash];
+    let command: CommandGetTrytes = CommandGetTrytes { command: "getTrytes".to_owned(), hashes: hashes};
 
     Some(Box::new(command))
 }
