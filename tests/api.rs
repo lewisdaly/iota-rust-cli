ee3b824e150af95bbbc20da13e0120799eb8da35
Lewis Daly <lewis.daly@enabled.com.au>
2018-03-05 08:21:35 +1030

    merge in fixed broken branch

--- tests/api.rs
+++ tests/api.rs
@@ -8,10 +8,11 @@ use iota_api::api_commands::FindTransactionsType;
 
 #[test]
 fn live_find_transactions() {
-    let address = "XFAELMMFKBKNGAYLKNFOLHMOTAM9IVOLABDLNKNWUIFK9QJXEJKGAUHMEPMYPGLPFBWZAXJMFZYCWMQEA";
+    // let address = "XFAELMMFKBKNGAYLKNFOLHMOTAM9IVOLABDLNKNWUIFK9QJXEJKGAUHMEPMYPGLPFBWZAXJMFZYCWMQEA";
+    let tag = "MY9CUSTOM9TAG99999999999999";
 
     let client = &IotaClient { protocol: "http://".to_owned(), host: "node.iota.com.tw".to_owned(), port: 5000 };
-    let result = match api::find_transactions(client, FindTransactionsType::Addresses, address.to_owned()) {
+    let result = match api::find_transactions(client, FindTransactionsType::Tags, tag.to_owned()) {
         Ok(n) => n,
         Err(err) => panic!("called `Result::unwrap()` on an `Err` value: {:?}", err),
     };
